a809b207f4014467bddfb09fefc62c36748d63ff
xia0z <939529983@qq.com>
2019-07-11 18:43:30 +0800

    add install file

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+shell_root_dir=$(pwd)
+shell_file_name="issh.sh"
+shell_file=$shell_root_dir"/"$shell_file_name
+
+bash_profile=$HOME"/.bash_profile"
+
+
+function xlog(){
+    echo "[log]: "$1
+}
+
+grep 'issh.sh' $bash_profile > /dev/null
+if [ $? -eq 0 ]; then
+    xlog $bash_profile" has include "$shell_file_name" just source it."
+    source $bash_profile
+    exit 0
+else
+    xlog "not install. gogogo..."
+fi
+
+
+echo -e "\nsource $shell_file" >> $bash_profile
+
+xlog "source "$bash_profile
+
+source $bash_profile > /dev/null
+
+xlog "done!"
