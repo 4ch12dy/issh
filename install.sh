c7d9c30d8702faa4167976e53910d3c7b0929800
xia0z <939529983@qq.com>
2019-07-12 11:31:12 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ shell_file_name="issh.sh"
 shell_file=$shell_root_dir"/"$shell_file_name
 
 bash_profile=$HOME"/.bash_profile"
-
+zsh_profile=$HOME"/.zshrc"
 
 
 function xlog(){
@@ -17,11 +17,11 @@ fi
 echo "$shell_root_dir" > ~/.issh/rootdir
 
 
+# add issh.sh to bash_profile
 grep 'issh.sh' $bash_profile > /dev/null
 if [ $? -eq 0 ]; then
     xlog $bash_profile" has include "$shell_file_name" just source it."
     source $bash_profile
-    exit 0
 else
     xlog "not install. gogogo..."
 fi
@@ -29,8 +29,23 @@ fi
 
 echo -e "\nsource $shell_file" >> $bash_profile
 
-xlog "source "$bash_profile
+
+# add issh.sh to bash_profile
+xlog "source "$zsh_profile
+
+grep 'issh.sh' $zsh_profile > /dev/null
+if [ $? -eq 0 ]; then
+    xlog $zsh_profile" has include "$shell_file_name" just source it."
+    source $zsh_profile
+else
+    xlog "not install. gogogo..."
+fi
+
+echo -e "\nsource $shell_file" >> $zsh_profile
+
+xlog "source "$zsh_profile
 
 source $bash_profile > /dev/null
+source $zsh_profile > /dev/null
 
 xlog "done!"
