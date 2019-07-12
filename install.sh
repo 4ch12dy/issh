b7e38f044c3c4a75a0bfbb037937162eeb4e8000
xia0z <939529983@qq.com>
2019-07-12 11:57:53 +0800

    install.sh support zsh

--- install.sh
+++ install.sh
@@ -16,36 +16,33 @@ if [[ ! -d ~/.issh ]]; then
 fi
 echo "$shell_root_dir" > ~/.issh/rootdir
 
+if [[ "$SHELL" = "/bin/zsh" ]]; then
+	# add issh.sh to bash_profile
+	xlog "source "$zsh_profile
+
+	grep 'issh.sh' $zsh_profile > /dev/null
+	if [ $? -eq 0 ]; then
+	    xlog $zsh_profile" has include "$shell_file_name" just source it."
+	else
+	    xlog "not install. gogogo..."
+	    echo -e "\nsource $shell_file" >> $zsh_profile
+	fi
+
+	zsh -c "source $zsh_profile > /dev/null"
+	xlog "done!"
+	exit
+fi
+
 
 # add issh.sh to bash_profile
+xlog "source "$bash_profile
 grep 'issh.sh' $bash_profile > /dev/null
 if [ $? -eq 0 ]; then
     xlog $bash_profile" has include "$shell_file_name" just source it."
-    source $bash_profile
 else
     xlog "not install. gogogo..."
+    echo -e "\nsource $shell_file" >> $bash_profile
 fi
-
-
-echo -e "\nsource $shell_file" >> $bash_profile
-
-
-# add issh.sh to bash_profile
-xlog "source "$zsh_profile
-
-grep 'issh.sh' $zsh_profile > /dev/null
-if [ $? -eq 0 ]; then
-    xlog $zsh_profile" has include "$shell_file_name" just source it."
-    source $zsh_profile
-else
-    xlog "not install. gogogo..."
-fi
-
-echo -e "\nsource $shell_file" >> $zsh_profile
-
-xlog "source "$zsh_profile
 
 source $bash_profile > /dev/null
-source $zsh_profile > /dev/null
-
 xlog "done!"
