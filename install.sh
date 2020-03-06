e54770c6d846fead85f3ba2ea825aba2ac37a45b
xia0 <939529983@qq.com>
2020-03-07 00:03:19 +0800

    re write install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-shell_root_dir=$(pwd)
-shell_file_name="issh.sh"
-shell_file=$shell_root_dir"/"$shell_file_name
+root_dir=$(pwd)
+issh_name="issh.sh"
+issh_shell_file="$root_dir/$issh_name"
+bash_profile="$HOME/.bash_profile"
+zsh_profile="$HOME/.zshrc"
 
-bash_profile=$HOME"/.bash_profile"
-zsh_profile=$HOME"/.zshrc"
-
-
-function xlog(){
-    echo "[log]: "$1
-}
-
+echo "==== install issh ===="
 if [[ ! -d ~/.issh ]]; then
     mkdir -p ~/.issh
 fi
 
-echo "$shell_root_dir" > ~/.issh/rootdir
+echo "$root_dir" > ~/.issh/rootdir
 
 if [[ "$SHELL" = "/bin/zsh" || "$SHELL" = "/usr/local/bin/zsh" ]]; then
 
@@ -26,21 +21,17 @@ elif [[ "$SHELL" = "/bin/bash" ]]; then
     sh_profile=$bash_profile
 
 else
-    echo "Not Support shell:$SHELL"
+    echo "[-] not Support shell:$SHELL"
     exit
 fi
 
+echo "[+] detect current shell profile: $sh_profile"
 
+sed -i "" '/source.*issh\.sh/d' $sh_profile 2>/dev/null
 # add issh.sh to shell_profile
-xlog "add \"source $shell_file\" to $sh_profile"
+echo "[*] add \"source $issh_shell_file\" to $sh_profile"
+echo -e "\nsource $issh_shell_file" >> $sh_profile
 
-grep 'issh.sh' $sh_profile > /dev/null
-if [ $? -eq 0 ]; then
-    xlog $sh_profile" has include "$shell_file_name" just source it."
-else
-    xlog "install..."
-    echo -e "\nsource $shell_file" >> $sh_profile
-fi
-
-# source $sh_profile > /dev/null
-xlog "Please Run command:source $sh_profile"
+# done 
+echo "[+] install finished, you can re-source $sh_profile or open a new terminal"
+echo "======================"
